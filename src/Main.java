6784fc4c4968f95a096a6f8a3d0a84bffa2591aa
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-20 10:37:45 -0500

    initial commit of day2 udemy thing

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        int passengers = 20;
+
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+
+    }
+}
